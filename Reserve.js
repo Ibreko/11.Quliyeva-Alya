e0d30c82bdd97f807c364e02a7425ac135372496
Ibreko <alizade.ibrahim1234@gmail.com>
2024-04-26 23:34:05 +0400

    new file:   -_Hotel_--removebg-preview.png 	new file:   Foto1.jpg 	new file:   Foto10.jpg 	new file:   Foto12.jpg 	new file:   Foto13.jpg 	new file:   Foto14.jpg 	new file:   Foto15.jpg 	new file:   Foto16.jpg 	new file:   Foto18.png 	new file:   Foto19.png 	new file:   Foto20.png 	new file:   Foto21.png 	new file:   Foto22.png 	new file:   Foto23.png 	new file:   Foto3.jpg 	new file:   Foto5.jpg 	new file:   Foto6.jpg 	new file:   Foto9.jpg 	new file:   Hotel.css 	new file:   Hotel.html 	new file:   Menu.css 	new file:   Menu.html 	new file:   Reserve.css 	new file:   Reserve.html 	new file:   Reserve.js 	new file:   foto11.jpg 	new file:   foto4.jpg 	new file:   klipartz.com (1).png 	new file:   nav.jpg 	new file:   ocean aerial view.mp4

---
+++ Reserve.js
@@ -0,0 +1,5 @@
+document.querySelector('.knopka').addEventListener('click', function() {
+    console.log('hello');
+    document.querySelector('.pole-reserve').style.opacity = '1';
+    document.querySelector('.knopka-rezerv').style.display = 'none';
+});
